e4bb1a30251c31a49113537c29a3d3a74e86f721
Grigoriy Romanyuk <grigoriyromanyuk@Grigoriys-MBP.grisha.local>
2019-11-22 21:51:45 -0800

    Now actually updating Shopify

--- script.py
+++ script.py
@@ -1,21 +1,11 @@
-from dataclasses import dataclass
-from typing import List
-from decimal import Decimal
-from ebaysdk.trading import Connection as Trading
 import datetime
-
+import requests
+import time
 import yaml
 
-
-@dataclass
-class InventoryItem:
-    sku: str
-    price: Decimal
-
-
-@dataclass
-class EbayInventoryItem(InventoryItem):
-    id: str
+from dataclasses import dataclass
+from decimal import Decimal, getcontext
+from ebaysdk.trading import Connection as Trading
 
 
 @dataclass
@@ -24,8 +14,9 @@ class Shop:
     type: str
     config: dict
     master: bool = False
+    coefficient: float = 1
 
-    def _get_ebay_items(self) -> List[EbayInventoryItem]:
+    def _get_ebay_items(self):
         result = []
         api = Trading(config_file=None, **self.config)
         page_number = 0
@@ -44,6 +35,7 @@ class Shop:
                         100,
                     "PageNumber": page_number}
             }).dict()
+
             items = page.get('ItemArray').get("Item")
             if type(items) != list:
                 items = [items]
@@ -55,32 +47,155 @@ class Shop:
                             item['ListingDetails']
                                 ['ConvertedStartPrice']['value']
                         ),
-                        item['ItemID']
+                        self,
+                        item['ItemID'],
                     )
                 )
+            max_page_number = int(page.get('PaginationResult')
+                                  .get('TotalNumberOfPages'))
         return result
 
-    def get_items(self) -> List[InventoryItem]:
+    def _get_shopify_items(self):
+        end_point_url = \
+            f"https://{self.name}.myshopify.com/admin/api/graphql.json"
+        headers = {
+            "X-Shopify-Access-Token": self.config["password"],
+            "X-GraphQL-Cost-Include-Fields": "true",
+            "Content-Type": "application/graphql"
+        }
+        cursor = ''
+        hasNextPage = True
+        results = []
+
+        while hasNextPage:
+            hasNextPage = False
+            payload = '''
+            {
+                productVariants(first:250%s) {
+                    edges {
+                        cursor
+                        node {
+                            id
+                            sku
+                            price
+                            displayName
+                        }
+                    }
+                    pageInfo {
+                        hasNextPage
+                    }
+                }
+            }
+            ''' % cursor
+            response = requests.post(
+                end_point_url,
+                data=payload,
+                headers=headers,
+            )
+            result = response.json()
+            if 'data' not in result:
+                print(result)
+            for item in result['data']['productVariants']['edges']:
+                cursor = f',after:"{item["cursor"]}"'
+                results.append(
+                    ShopifyInventoryItem(
+                        item['node']['sku'],
+                        Decimal(item['node']['price']),
+                        self,
+                        item['node']['id'],
+                    )
+                )
+            hasNextPage = bool(result['data']['productVariants'][
+                               'pageInfo']['hasNextPage'])
+            throttle_status = str(result['extensions']['cost']
+                                  ['throttleStatus']['currentlyAvailable'])
+            if int(throttle_status) < 300:
+                print("sleeping for 10 seconds...", len(results))
+                time.sleep(10)
+        return results
+
+    def get_items(self):
         if self.type == 'ebay':
             return self._get_ebay_items()
+        elif self.type == 'shopify':
+            return self._get_shopify_items()
         raise NotImplementedError(f"Unsupported type '{self.type}'s")
 
 
+@dataclass
+class InventoryItem:
+    sku: str
+    price: Decimal
+    shop: Shop
+
+
+@dataclass
+class ShopifyInventoryItem(InventoryItem):
+    id: str
+
+    def save(self):
+        end_point_url = \
+            f"https://{self.shop.name}.myshopify.com/admin/api/graphql.json"
+        headers = {
+            "X-Shopify-Access-Token": self.shop.config["password"],
+            "X-GraphQL-Cost-Include-Fields": "true",
+            "Content-Type": "application/graphql"
+        }
+        payload = f'''
+            mutation {{
+                productVariantUpdate(input: {{id: "{self.id}", price: "{self.price}" }}) {{
+                    productVariant {{
+                        id
+                    }}
+                }}
+            }}
+        '''
+        response = requests.post(
+            end_point_url,
+            data=payload,
+            headers=headers,
+        )
+        response.raise_for_status()
+
+
+@dataclass
+class EbayInventoryItem(InventoryItem):
+    id: str
+
+
+# TODO: Unit test this function
+def list_to_dict_by_sku(array, result=None):
+    if not result:
+        result = {}
+    for item in array:
+        if result.get(item.sku):
+            result[item.sku].append(item)
+        else:
+            result[item.sku] = [item]
+    return result
+
+
+getcontext().prec = 2
 config = yaml.safe_load(open('config.yml', 'r'))
 shops = [Shop(**shop) for shop in config.get("shops")]
+
 
 master = dict()
 synced = dict()
 for shop in shops:
     if shop.master:
-        for item in shop.get_items():
-            if master.get(item.sku):
-                master[item.sku].append(item)
-            else:
-                master[item.sku] = [item]
+        master = list_to_dict_by_sku(shop.get_items())
     else:
-        for item in shop.get_items():
-            if synced.get(item.sku):
-                synced[item.sku].append(item)
-            else:
-                synced[item.sku] = [item]
+        synced = list_to_dict_by_sku(shop.get_items(), synced)
+print("!")
+for sku, master_item in master.items():
+    for item in synced.get(sku, []):
+        print(item)
+        if abs(float(master_item[0].price) * item.shop.coefficient -
+               float(item.price)) > 0.1:
+            print(f"(SKU: {sku}) Update {item.id} to "
+                  f"{float(master_item[0].price) * item.shop.coefficient} "
+                  f"from {item.price}")
+            item.price = \
+                Decimal(float(master_item[0].price) * item.shop.coefficient)
+            item.save()
